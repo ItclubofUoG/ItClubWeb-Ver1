cf6b826f6a12bbe928f8e3f67480190999e1af12
Lê Chí Luân <chiluan6601@gmail.com>
2022-06-29 10:12:23 +0700

    update

--- ChangeInfor_Pro.php
+++ ChangeInfor_Pro.php
@@ -17,6 +17,7 @@ if (isset($_GET['func']) && $_GET['func'] == 'update') {
             }
         }
     }
+    
     $stid = $_POST['stid'];
     $checkstid = mysqli_query($conn, "SELECT * FROM users WHERE id='$id'") or die(mysqli_error($conn));
     while ($row2 = mysqli_fetch_array($checkstid, MYSQLI_ASSOC)) {
