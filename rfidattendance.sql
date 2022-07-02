6f19a8c52c55e9e0b4c6f1ff54dd1addbd9d2773
Lê Chí Luân <chiluan6601@gmail.com>
2022-07-02 11:07:58 +0700

    Update rfidattendance.sql
    Cập nhật database lại. Nguyên nhân do devide_uid giữa table device and users phải trùng nhau vì vậy không nên reload token của device.

--- rfidattendance.sql
+++ rfidattendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2022 at 11:24 AM
+-- Generation Time: Jul 02, 2022 at 06:05 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -39,7 +39,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `admin_name`, `admin_email`, `admin_pwd`) VALUES
-(1, 'admin', 'admin@gmail.com', '25d55ad283aa400af464c76d713c07ad');
+(1, 'admin', 'admin@gmail.com', '25d55ad283aa400af464c76d713c07ad'),
+(2, 'It Club', 'itclubofgwu@gmail.com', '77cd7d41ab4510b814e17dd13aed876b');
 
 -- --------------------------------------------------------
 
@@ -61,7 +62,7 @@ CREATE TABLE `devices` (
 --
 
 INSERT INTO `devices` (`id`, `device_name`, `device_dep`, `device_uid`, `device_date`, `device_mode`) VALUES
-(6, 'Devece 1', 'IT Department', 'd4b702c9fa58b983', '2022-05-27', 1);
+(6, 'Devece 1', 'IT Department', '1e6cb94e4dc1adbe', '2022-05-27', 1);
 
 -- --------------------------------------------------------
 
@@ -116,35 +117,35 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `serialnumber`, `gender`, `email`, `card_uid`, `card_select`, `user_date`, `device_uid`, `device_dep`, `add_card`, `Password`, `StudentID`, `Department`, `github`, `total_scores`) VALUES
-(62, 'DÆ°Æ¡ng KhÃ¡nh Ngá»c ', 0, 'female', 'ngocdkgcc19167@fpt.edu.vn', '325015126', 0, '2001-03-04', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC19167', 'Media', 'KhanhNgocNe', 0),
-(63, 'LÃª Trung KiÃªn', 0, 'male', 'kienltgcc19138@fpt.edu.vn', '8319713126', 0, '2001-02-03', 'a6c23bd95b9832dd', 'IT Department', 1, '30240ebd7922b374ae652efcfb8d9a66', 'GCC19138', 'Network', 'kienltgcc19138', 0),
-(64, 'Tráº§n TrÃºc PhÆ°Æ¡ng', 0, 'female', 'phuongttgcc200245@fpt.edu.vn', '14714812726', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200245', 'Design', NULL, 0),
-(65, 'TrÆ°Æ¡ng Quang Minh', 0, 'male', 'minhtqgcc200321@fpt.edu.vn', '24321813526', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, 'a820bf8bd9eecc890280326fdfb4cd5c', 'GCC200321', 'Font-end', 'MinhGCC200321', 0),
-(66, 'Nguyá»…n BÃ¡ Lá»™c', 0, 'male', 'baloc2000ct@gmail.com', '5116213126', 0, '2000-03-30', 'a6c23bd95b9832dd', 'IT Department', 1, 'af57c2afb7db0b0e46d0c7d6559578b1', 'GCC18053', 'Design', 'baloc2000ct', 0),
-(67, 'Nguyá»…n ChÃ­ Háº£i', 0, 'male', 'haincgcc18033@fpt.edu.vn', '1919612226', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC18033', 'Back-end', NULL, 0),
-(68, 'Nguyá»…n Duy Quang ', 0, 'male', 'quangndgcc200030@fpt.edu.vn', '316613026', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200030', 'Design', NULL, 0),
-(69, 'Äá»— Há»¯u Duy', 0, 'male', 'duydhgcc200018@fpt.edu.vn', '196614526', 0, '2002-07-10', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200018', 'Back-end', 'carlduy2002', 0),
-(70, 'Há»“ Kiáº¿n Vinh', 0, 'male', 'vinhhkgcc200101@fpt.edu.vn', '16311313426', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '6b45302dda6f2ac7d3d1254641f91eb9', 'GCC200101', 'Design', NULL, 0),
-(71, 'Huá»³nh Quá»‘c DÆ°Æ¡ng', 0, 'male', 'duonghqgcc19037@fpt.edu.vn', '14710218724', 0, '2001-11-04', 'a6c23bd95b9832dd', 'IT Department', 1, '0621029d62ebdac321a26bf135a84837', 'GCC19037', 'Network', 'Duongdot', 0),
-(73, 'Nguyá»…n VÄƒn Láº­p', 0, 'male', 'lapnvgcc200247@fpt.edu.vn', '673012526', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200247', 'Design', NULL, 0),
-(74, 'Nguyá»…n Nháº¥t ChÃ­nh', 0, 'male', 'chinhnngcc200312@fpt.edu.vn', '14712013926', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200312', 'Design', NULL, 0),
-(75, 'Nguyá»…n Tiáº¿n Äá»©c ', 0, 'male', 'ducntgcc19107@fpt.edu.vn', '2436814826', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '35b1df8c572c7e0a66853564d9e33b34', 'GCC19107', 'Design', NULL, 0),
-(76, 'DÆ°Æ¡ng VÅ© TÆ°á»ng', 0, 'male', 'tuongdvgcc19257@fpt.edu.vn', '321111926', 0, '2001-12-18', 'a6c23bd95b9832dd', 'IT Department', 1, '27e3771f694c02fa49cb43ee860e2e55', 'GCC19257', 'Back-end', 'Tuong2001', 0),
-(77, 'Tráº§n Ngá»c TÄƒng', 0, 'male', 'tangtngcc200110@fpt.edu.vn', '35313526', 0, '2002-10-19', 'a6c23bd95b9832dd', 'IT Department', 1, '2c7cc940efa1bb6ed164fb142a0e3a37', 'GCC200110', 'Design', 'Dann2922', 0),
-(78, 'Nguyá»…n SiÃªu', 0, 'male', 'sieungcc19186@fpt.edu.vn', '323512426', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC19186', 'Design', NULL, 0),
-(79, 'LÃª ChÃ­ Kháº£i', 0, 'male', 'khailcgcc19095@fpt.edu.vn', '6717016224', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC19095', 'Back-end', NULL, 0),
-(80, 'LÃª ChÃ­ LuÃ¢n', 0, 'male', 'luanlcgcc19023@fpt.edu.vn', '195015026', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '51c0767e4d9588b222c2c8e21d7f07b1', 'GCC19023', 'Back-end', 'Lechiluan', 0),
-(81, 'Nguyá»…n Ngá»c Nháº«n', 0, 'female', 'nhannngcc19209@fpt.edu.vn', '192213326', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25093d99f822618802b3bff5000d5972', 'GCC19209', 'Media', NULL, 0),
-(82, 'Nguyá»…n ThÃ nh Äáº¡t', 0, 'male', 'datntgcc19213@fpt.edu.vn', '21116414926', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '216a3bcfc8281c1d4f16575a6ea97555', 'GCC19213', 'Font-end', 'datnt19213', 0),
-(83, 'Äáº·ng Nguyá»…n ÄÄƒng KhÃ´i', 0, 'male', 'khoidndgcc200058@fpt.edu.vn', '1912913626', 0, '2002-08-20', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200058', 'Back-end', 'DKhoi02', 0),
-(84, 'Tráº§n PhÆ°á»›c Háº£o', 0, 'male', 'haotpgcc19259@fpt.edu.vn', '19515213926', 0, '2001-11-09', 'a6c23bd95b9832dd', 'IT Department', 1, 'fcea920f7412b5da7be0cf42b8c93759', 'GCC19259', 'Back-end', 'phuochao091101', 0),
-(85, 'Tráº§n Tháº£o Trung', 0, 'male', 'trungttgcc200091@fpt.edu.vn', '992413426', 0, '2002-08-05', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200091', 'Design', 'trungcenter25', 0),
-(86, 'Huá»³nh Nháº­t TÃ¢n', 0, 'male', 'tanhngcc200156@fpt.edu.vn', '7927104', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200156', 'Design', NULL, 0),
-(87, 'Há»“ Nháº¥t Khoa', 0, 'male', 'khoahngcc210023@fpt.edu.vn', '13121917224', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, 'e1de3d0c72beecb6e6b84c38b597a47d', 'GCC210023', 'Design', 'KhoaHN612', 0),
-(88, 'LÆ°u HoÃ i Phong', 0, 'male', 'phonglhgcc19022@fpt.edu.vn', '1916317024', 0, '2001-03-01', 'a6c23bd95b9832dd', 'IT Department', 1, 'e17aef70ce36bfcae19de20cacd51be3', 'GCC19022', 'Back-end', 'luuhoaiphong', 0),
-(90, 'Nguyá»…n Trung NguyÃªn', 0, 'male', 'nguyenntgcc19275@fpt.edu.vn', '1798114826', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, 'f9c81dd99f5aec80fd33bb7964fb7267', 'GCC19275', 'Network', 'zolmkoz', 0),
-(91, 'Nguyá»…n CÃ´ng Thá»ƒ ', 0, 'male', 'thencgcc200104@fpt.edu.vn', '19519513326', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200104', 'Font-end', NULL, 0),
-(92, 'Nguyá»…n PhÃº CÆ°á»ng', 0, 'male', 'cuongnp3110@gmail.com', '3518214826', 0, '2022-05-27', 'a6c23bd95b9832dd', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC18035', 'Back-end', NULL, 0),
+(62, 'DÆ°Æ¡ng KhÃ¡nh Ngá»c ', 0, 'female', 'ngocdkgcc19167@fpt.edu.vn', '325015126', 0, '2001-03-04', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC19167', 'Media', 'KhanhNgocNe', 0),
+(63, 'LÃª Trung KiÃªn', 0, 'male', 'kienltgcc19138@fpt.edu.vn', '8319713126', 0, '2001-02-03', '1e6cb94e4dc1adbe', 'IT Department', 1, '30240ebd7922b374ae652efcfb8d9a66', 'GCC19138', 'Network', 'kienltgcc19138', 0),
+(64, 'Tráº§n TrÃºc PhÆ°Æ¡ng', 0, 'female', 'phuongttgcc200245@fpt.edu.vn', '14714812726', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200245', 'Design', NULL, 0),
+(65, 'TrÆ°Æ¡ng Quang Minh', 0, 'male', 'minhtqgcc200321@fpt.edu.vn', '24321813526', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, 'a820bf8bd9eecc890280326fdfb4cd5c', 'GCC200321', 'Font-end', 'MinhGCC200321', 0),
+(66, 'Nguyá»…n BÃ¡ Lá»™c', 0, 'male', 'baloc2000ct@gmail.com', '5116213126', 0, '2000-03-30', '1e6cb94e4dc1adbe', 'IT Department', 1, 'af57c2afb7db0b0e46d0c7d6559578b1', 'GCC18053', 'Design', 'baloc2000ct', 0),
+(67, 'Nguyá»…n ChÃ­ Háº£i', 0, 'male', 'haincgcc18033@fpt.edu.vn', '1919612226', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC18033', 'Back-end', NULL, 0),
+(68, 'Nguyá»…n Duy Quang ', 0, 'male', 'quangndgcc200030@fpt.edu.vn', '316613026', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200030', 'Design', NULL, 0),
+(69, 'Äá»— Há»¯u Duy', 0, 'male', 'duydhgcc200018@fpt.edu.vn', '196614526', 0, '2002-07-10', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200018', 'Back-end', 'carlduy2002', 0),
+(70, 'Há»“ Kiáº¿n Vinh', 0, 'male', 'vinhhkgcc200101@fpt.edu.vn', '16311313426', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '6b45302dda6f2ac7d3d1254641f91eb9', 'GCC200101', 'Design', NULL, 0),
+(71, 'Huá»³nh Quá»‘c DÆ°Æ¡ng', 0, 'male', 'duonghqgcc19037@fpt.edu.vn', '14710218724', 0, '2001-11-04', '1e6cb94e4dc1adbe', 'IT Department', 1, '0621029d62ebdac321a26bf135a84837', 'GCC19037', 'Network', 'Duongdot', 0),
+(73, 'Nguyá»…n VÄƒn Láº­p', 0, 'male', 'lapnvgcc200247@fpt.edu.vn', '673012526', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200247', 'Design', NULL, 0),
+(74, 'Nguyá»…n Nháº¥t ChÃ­nh', 0, 'male', 'chinhnngcc200312@fpt.edu.vn', '14712013926', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200312', 'Design', NULL, 0),
+(75, 'Nguyá»…n Tiáº¿n Äá»©c ', 0, 'male', 'ducntgcc19107@fpt.edu.vn', '2436814826', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '35b1df8c572c7e0a66853564d9e33b34', 'GCC19107', 'Design', NULL, 0),
+(76, 'DÆ°Æ¡ng VÅ© TÆ°á»ng', 0, 'male', 'tuongdvgcc19257@fpt.edu.vn', '321111926', 0, '2001-12-18', '1e6cb94e4dc1adbe', 'IT Department', 1, '27e3771f694c02fa49cb43ee860e2e55', 'GCC19257', 'Back-end', 'Tuong2001', 0),
+(77, 'Tráº§n Ngá»c TÄƒng', 0, 'male', 'tangtngcc200110@fpt.edu.vn', '35313526', 0, '2002-10-19', '1e6cb94e4dc1adbe', 'IT Department', 1, '2c7cc940efa1bb6ed164fb142a0e3a37', 'GCC200110', 'Design', 'Dann2922', 0),
+(78, 'Nguyá»…n SiÃªu', 0, 'male', 'sieungcc19186@fpt.edu.vn', '323512426', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC19186', 'Design', NULL, 0),
+(79, 'LÃª ChÃ­ Kháº£i', 0, 'male', 'khailcgcc19095@fpt.edu.vn', '6717016224', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC19095', 'Back-end', NULL, 0),
+(80, 'Lê Chí Luân', 0, 'male', 'luanlcgcc19023@fpt.edu.vn', '195015026', 1, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, 'e10adc3949ba59abbe56e057f20f883e', 'GCC19023', 'Back-end', 'Lechiluan', 0),
+(81, 'Nguyá»…n Ngá»c Nháº«n', 0, 'female', 'nhannngcc19209@fpt.edu.vn', '192213326', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25093d99f822618802b3bff5000d5972', 'GCC19209', 'Media', NULL, 0),
+(82, 'Nguyá»…n ThÃ nh Äáº¡t', 0, 'male', 'datntgcc19213@fpt.edu.vn', '21116414926', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '216a3bcfc8281c1d4f16575a6ea97555', 'GCC19213', 'Font-end', 'datnt19213', 0),
+(83, 'Äáº·ng Nguyá»…n ÄÄƒng KhÃ´i', 0, 'male', 'khoidndgcc200058@fpt.edu.vn', '1912913626', 0, '2002-08-20', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200058', 'Back-end', 'DKhoi02', 0),
+(84, 'Tráº§n PhÆ°á»›c Háº£o', 0, 'male', 'haotpgcc19259@fpt.edu.vn', '19515213926', 0, '2001-11-09', '1e6cb94e4dc1adbe', 'IT Department', 1, 'fcea920f7412b5da7be0cf42b8c93759', 'GCC19259', 'Back-end', 'phuochao091101', 0),
+(85, 'Tráº§n Tháº£o Trung', 0, 'male', 'trungttgcc200091@fpt.edu.vn', '992413426', 0, '2002-08-05', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200091', 'Design', 'trungcenter25', 0),
+(86, 'Huá»³nh Nháº­t TÃ¢n', 0, 'male', 'tanhngcc200156@fpt.edu.vn', '7927104', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200156', 'Design', NULL, 0),
+(87, 'Há»“ Nháº¥t Khoa', 0, 'male', 'khoahngcc210023@fpt.edu.vn', '13121917224', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, 'e1de3d0c72beecb6e6b84c38b597a47d', 'GCC210023', 'Design', 'KhoaHN612', 0),
+(88, 'LÆ°u HoÃ i Phong', 0, 'male', 'phonglhgcc19022@fpt.edu.vn', '1916317024', 0, '2001-03-01', '1e6cb94e4dc1adbe', 'IT Department', 1, 'e17aef70ce36bfcae19de20cacd51be3', 'GCC19022', 'Back-end', 'luuhoaiphong', 0),
+(90, 'Nguyá»…n Trung NguyÃªn', 0, 'male', 'nguyenntgcc19275@fpt.edu.vn', '1798114826', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, 'f9c81dd99f5aec80fd33bb7964fb7267', 'GCC19275', 'Network', 'zolmkoz', 0),
+(91, 'Nguyá»…n CÃ´ng Thá»ƒ ', 0, 'male', 'thencgcc200104@fpt.edu.vn', '19519513326', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200104', 'Font-end', NULL, 0),
+(92, 'Nguyá»…n PhÃº CÆ°á»ng', 0, 'male', 'cuongnp3110@gmail.com', '3518214826', 0, '2022-05-27', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC18035', 'Back-end', NULL, 0),
 (95, 'Tháº¡ch Ngá»c Trung', 0, 'male', 'trungtngcc19009@fpt.edu.vn', '17922114226', 0, '2001-02-02', '1e6cb94e4dc1adbe', 'IT Department', 1, 'd3686f79f1cf0ceb6e0c388930a4f0ae', 'GCC19009', 'Font-end', 'trungthach020201', 0),
 (96, 'Äá»— PhÃº CÆ°á»ng', 0, 'male', 'cuongdpgcc200122@fpt.edu.vn', '9915712226', 0, '2002-06-23', '1e6cb94e4dc1adbe', 'IT Department', 1, 'b79f11000d8b86833f8d55d6349be415', 'GCC200122', 'Back-end', 'SkullCuong#106647536', 0),
 (97, 'LÃª Nguyá»…n Nhá»±t KhÃ´i', 0, 'male', 'khoilnngcc210092@fpt.edu.vn', '17918116624', 0, '2022-06-01', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC210092', 'Media', 'None', 0),
@@ -158,7 +159,7 @@ INSERT INTO `users` (`id`, `username`, `serialnumber`, `gender`, `email`, `card_
 (105, 'None', 0, 'female', 'loiltgcc210019@fpt.edu.vn', '1958112726', 0, '2022-06-01', '1e6cb94e4dc1adbe', 'IT Department', 1, '2348f8d9703b493c82ddab6192a893e4', 'GCC210019', 'Media', 'none', 0),
 (106, 'VÆ°Æ¡ng Quang Má»‹nh', 0, 'female', 'minhvqgcc210229@fpt.edu.vn', '22710713226', 0, '2003-09-03', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC210229', 'Network', 'none', 0),
 (107, 'None', 0, 'female', 'thinhhtgcc210152@fpt.edu.vn', '8312911926', 0, '2022-06-01', '1e6cb94e4dc1adbe', 'IT Department', 1, '2348f8d9703b493c82ddab6192a893e4', 'GCC210152', 'Media', 'none', 0),
-(108, 'Nguyá»…n Quá»‘c ThÃ´ng', 0, 'male', 'thongnguyennqt@gmail.com', '13117613626', 1, '2022-11-07', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200003', 'Back-end', ' ngquocthong', 0);
+(108, 'Nguyá»…n Quá»‘c ThÃ´ng', 0, 'male', 'thongnguyennqt@gmail.com', '13117613626', 0, '2022-11-07', '1e6cb94e4dc1adbe', 'IT Department', 1, '25d55ad283aa400af464c76d713c07ad', 'GCC200003', 'Back-end', ' ngquocthong', 0);
 
 -- --------------------------------------------------------
 
@@ -205,7 +206,8 @@ INSERT INTO `users_logs` (`id`, `username`, `serialnumber`, `card_uid`, `device_
 (118, 'None', 0, '992413426', '8f19e31055c56b05', 'DTVT', '2022-05-23', '10:25:51', '10:26:01', 1),
 (119, 'None', 0, '15171104178', '8f19e31055c56b05', 'DTVT', '2022-05-23', '10:27:00', '00:00:00', 0),
 (120, 'None', 0, '992413426', '8f19e31055c56b05', 'DTVT', '2022-05-23', '10:27:03', '10:32:13', 1),
-(121, 'None', 0, '992413426', '8f19e31055c56b05', 'DTVT', '2022-05-23', '10:32:13', '00:00:00', 0);
+(121, 'None', 0, '992413426', '8f19e31055c56b05', 'DTVT', '2022-05-23', '10:32:13', '00:00:00', 0),
+(242, 'Lê Chí Luân', 0, '195015026', '1e6cb94e4dc1adbe', 'IT Department', '2022-07-02', '11:04:48', '00:00:00', 0);
 
 --
 -- Indexes for dumped tables
@@ -249,7 +251,7 @@ ALTER TABLE `users_logs`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `devices`
@@ -273,7 +275,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users_logs`
 --
 ALTER TABLE `users_logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=241;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=243;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
