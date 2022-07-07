600691fb0208b2a65d43d2c0f89cd0eae4f8ec12
Duongdot <babyduong29@gmail.com>
2022-07-07 16:43:31 +0700

    DoneLayoutEvent

--- Event.php
+++ Event.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
  <html lang="en">
  <head>
@@ -7,6 +5,9 @@
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="./css/User/event.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
  </head>
  <body style="background-color:  #0c5774;">
      <div class="skin">
@@ -14,70 +15,48 @@
              <img class="top-fig-content" src="./image/top.jpg" alt="">
          </div>
          <div class="tree-body">
-             <div class="tree-name"><strong>Monthly Event</strong></div>             
-                <?php
-                    //Display Event with Current Month and default value if there is not enough event
-                    include_once("connectDB.php");
-                    $current_month = 4;//date('m');
-                    $result = mysqli_query($conn, "SELECT * FROM event where month(date) = '$current_month'");
-                    $default = mysqli_query($conn, "SELECT * FROM event where id = 0");
-                    $default_row= mysqli_fetch_array($default, MYSQLI_ASSOC);
-                    $num_rows = mysqli_num_rows($result);
-                    $rowContent = array($default_row, $default_row, $default_row);
-                    $i = 0;
-                    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                        $rowContent[$i] = $row;
-                        $i++;
-                    }
-                ?>                          
-             <form action="" class="tree-form">
-                 <!-- event 1 -->
-                 
-                 <div class="tree-form-item">
-                     <div class="tree-item-child tree-image-cg">
-                         <img class="tree-image" src="./image/<?php echo $rowContent[0]['img_event'];?>" alt="">
-                     </div>
-                     <?php echo $rowContent[0]['img_event'];?>
-                 </div>
-                 <div class="tree-form-item">
-                     <div class="tree-item-child tree-item-cg">
-                         <div class="tree-child-tag">Title: <?php echo $rowContent[0]['title'];?></div>
-                         <div class="tree-child-tag">Time: <?php echo $rowContent[0]['time'];?></div>
-                         <div class="tree-child-tag">Location: <?php echo $rowContent[0]['location'];?></div>
-                         <div class="tree-child-tag">Description: <?php echo $rowContent[0]['description'];?></div>
-                     </div>
-                 </div>
-                 <!-- event 2 -->
-                 <div class="tree-form-item">
-                     <div class="tree-item-child tree-item-cg">
-                         <div class="tree-child-tag">Title: <?php echo $rowContent[1]['title'];?></div>
-                         <div class="tree-child-tag">Time: <?php echo $rowContent[1]['time'];?></div>
-                         <div class="tree-child-tag">Location: <?php echo $rowContent[1]['location'];?></div>
-                         <div class="tree-child-tag">Description: <?php echo $rowContent[1]['description'];?></div> 
-                     </div>
-                 </div>
-                 <div class="tree-form-item">
-                     <div class="tree-item-child tree-image-cg">
-                         <img class="tree-image" src="./image/<?php echo $rowContent[1]['img_event'];?>" alt="">
-                     </div>                     
-                 </div>
+             <div class="tree-name"><strong>Monthly Event</strong></div>                                      
+        
+             <div class="et-piece">
+                <img src="Event Image/DemoEventHome.png" alt="450x300" class="et-figure p1">
+                <div class="et-piece-info p2">
+                    <div class="et-detail">Title: </div>
+                    <div class="et-detail">Time: </div>
+                    <div class="et-detail">Location: </div>
+                    <div class="et-detail">Description: </div>
+                    <!-- The Button GET $id on the link href to show description in the popup description of each event -->
+                    <a class="et-more-info" href="#" >Show more</a>
+                    <div class="et-detail">Score: </div>
+                </div>
+            </div>
 
-                 <!-- event 3 -->
-                 <div class="tree-form-item">
-                     <div class="tree-item-child tree-image-cg">
-                         <img class="tree-image" src="./image/<?php echo $rowContent[2]['img_event'];?>" alt="">
-                     </div>
-                 </div>
-                 <div class="tree-form-item">
-                     <div class="tree-item-child tree-item-cg">
-                         <div class="tree-child-tag">Title: <?php echo $rowContent[2]['title'];?></div>
-                         <div class="tree-child-tag">Time: <?php echo $rowContent[2]['time'];?></div>
-                         <div class="tree-child-tag">Location: <?php echo $rowContent[2]['location'];?></div>
-                         <div class="tree-child-tag">Description:  <?php echo $rowContent[2]['description'];?></div>
-                         
-                     </div>
-                 </div>                 
-             </form>
+            <!-- The event piece has picture right -->
+            <div class="et-piece">
+                <img src="Event Image/DemoEventHome.png" alt="450x300" class="et-figure p2">
+                <div class="et-piece-info p4">
+                    <div class="et-detail">Title: </div>
+                    <div class="et-detail">Time: </div>
+                    <div class="et-detail">Location: </div>
+                    <div class="et-detail">Description: </div>
+                    <!-- The Button GET $id on the link href to show description in the popup description of each event -->
+                    <a class="et-more-info" href="#">Show more</a>
+                    <div class="et-detail">Score: </div>
+                </div>
+            </div>
+
+            <div class="et-piece">
+                <img src="Event Image/DemoEventHome.png" alt="450x300" class="et-figure p1">
+                <div class="et-piece-info p2">
+                    <div class="et-detail">Title: </div>
+                    <div class="et-detail">Time: </div>
+                    <div class="et-detail">Location: </div>
+                    <div class="et-detail">Description: </div>
+                    <!-- The Button GET $id on the link href to show description in the popup description of each event -->
+                    <a class="et-more-info" href="#" >Show more</a>
+                    <div class="et-detail">Score: </div>
+                </div>
+            </div>
+
          </div>
      </div>
  </body>
