660e7c5b208979c9fd1f326013e5c97bcff1510f
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-07-20 16:10:39 +0700

    fixed

--- Export_Excel.php
+++ Export_Excel.php
@@ -6,14 +6,14 @@ session_start();
 $output = '';
 
 if (isset($_POST['To_Excel'])) {
-
+    require 'connectDB.php';
     $searchQuery = " ";
     $Start_date = " ";
     $End_date = " ";
     $Start_time = " ";
     $End_time = " ";
-    $card_sel = " ";
-    $check = 0;
+    $Card_sel = " ";
+
     if ($_POST['date_sel_start'] != 0 || $_POST['date_sel_end'] != 0 || $_POST['card_sel'] != 0) {
         $check = 1;
     }
@@ -26,57 +26,60 @@ if (isset($_POST['To_Excel'])) {
         $_SESSION['searchQuery'] = "checkindate='" . date("Y-m-d") . "'";
     }
     //End date filter
-    if ($_POST['date_sel_end'] != 0) {
+    if ($_POST['date_sel_end'] != "") {
         $End_date = $_POST['date_sel_end'];
         $_SESSION['searchQuery'] = "checkindate BETWEEN '" . $Start_date . "' AND '" . $End_date . "'";
     } else {
         $End_date = date("Y-m-d");
-        $_SESSION['searchQuery'] = "checkindate BETWEEN '" . $Start_date . "' AND '" . $End_date . "'";
+        $_SESSION['searchQuery'] = "checkindate BETWEEN '" . $Start_date . "' AND '" . date("Y-m-d") . "'";
     }
-
-    // if (isset($_POST['time_sel'])) {
-    //     if ($_POST['time_sel'] == "Time_in") {
-    //         //Start time filter
-    //         if ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] == 0) {
-    //             $Start_time = $_POST['time_sel_start'];
-    //             $_SESSION['searchQuery'] .= " AND timein='" . $Start_time . "'";
-    //         } elseif ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] != 0) {
-    //             $Start_time = $_POST['time_sel_start'];
-    //         }
-    //         //End time filter
-    //         if ($_POST['time_sel_end'] != 0) {
-    //             $End_time = $_POST['time_sel_end'];
-    //             $_SESSION['searchQuery'] .= " AND timein BETWEEN '" . $Start_time . "' AND '" . $End_time . "'";
-    //         }
+    // //Time-In filter
+    // if ($_POST['time_sel'] == "Time_in") {
+    //     //Start time filter
+    //     if ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] == 0) {
+    //         $Start_time = $_POST['time_sel_start'];
+    //         $_SESSION['searchQuery'] .= " AND timein='" . $Start_time . "'";
+    //     } elseif ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] != 0) {
+    //         $Start_time = $_POST['time_sel_start'];
     //     }
-    //     //Time-out filter
-    //     if ($_POST['time_sel'] == "Time_out") {
-    //         //Start time filter
-    //         if ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] == 0) {
-    //             $Start_time = $_POST['time_sel_start'];
-    //             $_SESSION['searchQuery'] .= " AND timeout='" . $Start_time . "'";
-    //         } elseif ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] != 0) {
-    //             $Start_time = $_POST['time_sel_start'];
-    //         }
-    //         //End time filter
-    //         if ($_POST['time_sel_end'] != 0) {
-    //             $End_time = $_POST['time_sel_end'];
-    //             $_SESSION['searchQuery'] .= " AND timeout BETWEEN '" . $Start_time . "' AND '" . $End_time . "'";
-    //         }
+    //     //End time filter
+    //     if ($_POST['time_sel_end'] != 0) {
+    //         $End_time = $_POST['time_sel_end'];
+    //         $_SESSION['searchQuery'] .= " AND timein BETWEEN '" . $Start_time . "' AND '" . $End_time . "'";
     //     }
     // }
-    // //Time-In filter
-
+    // //Time-out filter
+    // if ($_POST['time_sel'] == "Time_out") {
+    //     //Start time filter
+    //     if ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] == 0) {
+    //         $Start_time = $_POST['time_sel_start'];
+    //         $_SESSION['searchQuery'] .= " AND timeout='" . $Start_time . "'";
+    //     } elseif ($_POST['time_sel_start'] != 0 && $_POST['time_sel_end'] != 0) {
+    //         $Start_time = $_POST['time_sel_start'];
+    //     }
+    //     //End time filter
+    //     if ($_POST['time_sel_end'] != 0) {
+    //         $End_time = $_POST['time_sel_end'];
+    //         $_SESSION['searchQuery'] .= " AND timeout BETWEEN '" . $Start_time . "' AND '" . $End_time . "'";
+    //     }
+    // }
     //Card filter
     if ($_POST['card_sel'] != 0) {
-        $card_sel = $_POST['card_sel'];
-        $_SESSION['searchQuery'] .= " AND card_uid='" . $card_sel . "'";
+        $Card_sel = $_POST['card_sel'];
+        $_SESSION['searchQuery'] .= " AND card_uid='" . $Card_sel . "'";
     }
-    //Department filter
-    // if ($_POST['dev_sel'] != 0) {
-    //     $dev_uid = $_POST['dev_sel'];
+    // //Department filter
+    // if ($_POST['dev_uid'] != 0) {
+    //     $dev_uid = $_POST['dev_uid'];
     //     $_SESSION['searchQuery'] .= " AND device_uid='" . $dev_uid . "'";
     // }
+
+
+    // if ($_POST['select_date'] == 1) {
+    //     $Start_date = date("Y-m-d");
+    //     $_SESSION['searchQuery'] = "checkindate='" . $Start_date . "'";
+    // }
+    
     $sql = "SELECT * FROM users_logs ORDER BY id DESC";
     if (isset($_SESSION['searchQuery']) && $check == 1) { //
         $sql = "SELECT * FROM users_logs WHERE " . $_SESSION['searchQuery'] . " ORDER BY id DESC";
